da02d5be56a101b8aa34ab4cb0e2ce3147355b61
Antonio <antonpogoryelov@gmail.com>
2024-06-27 14:08:42 +0300

    get_price
    added

--- Store_manager.py
+++ Store_manager.py
@@ -1,6 +1,19 @@
-class Store():
+class Store:
     def __init__(self, name='Магаз', address='Местный', items={}):
         self.name = name
         self.address = address
         self.items = items
-        
+
+    def get_price(self, item):
+        if item in self.items:
+            print(f'Магазин "{self.name}: цена за одну единицу "{item}" - {self.items[item]}')
+        else:
+            print(f'Магазин "{self.name}: товара "{item}" нет в ассортименте')
+
+
+
+
+
+
+
+
