a3350d0059dc664084aaaffa6a2182c741568464
Antonio <antonpogoryelov@gmail.com>
2024-06-27 13:49:01 +0300

    item_list
    item_list method was added

--- Store_manager.py
+++ Store_manager.py
@@ -8,7 +8,7 @@ class Store():
     def add_item(self, item, price=0):
         if item not in self.items.keys():
             self.items[item] = price
-            print(f'Магазин "{self.name}: товар "{item}" добавлен в ассортимент по цене {price}')
+            print(f'Магазин "{self.name}": товар "{item}" добавлен в ассортимент по цене {price}')
         else:
             print(f'Магазин "{self.name}": товар с наименованием "{item}" уже есть в ассортименте по цене '
                   f'{self.items[item]}')
@@ -17,7 +17,14 @@ class Store():
         if item in self.items:
             del self.items[item]
             print(f'Магазин "{self.name}": товар "{item}" удалён из ассортимента.')
-        else: print(f'Магазин "{self.name}: так товара с названием "{item}" и не было в наличии.')
+        else: print(f'Магазин "{self.name}": так товара с названием "{item}" и не было в наличии.')
+
+    def items_list(self):
+        print(f'Магазин "{self.name}", вот что мы предлагаем нашим преданным клиентам:')
+        if self.items is None: print('Упс, пока что ничего:(')
+        else:
+            for item, price in self.items.items():
+                print(f'{item} - {price}')
 
 
 
@@ -30,7 +37,3 @@ class Store():
 
 
 
-
-
-
-            )
