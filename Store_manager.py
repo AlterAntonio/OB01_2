836340907ecc019728bba09d787a6d853b13557e
Antonio <antonpogoryelov@gmail.com>
2024-06-27 14:24:38 +0300

    update_price
    method update_price was added

--- Store_manager.py
+++ Store_manager.py
@@ -10,7 +10,19 @@ class Store:
         else:
             print(f'Магазин "{self.name}: товара "{item}" нет в ассортименте')
 
-
+    def update_price(self, item, price):
+        if item in self.items:
+            if self.items[item] > price:
+                print('(Тревога, зафиксировано снижение цены!)')
+                before = self.items[item]
+                discount = (before - price) / before * 100
+                print(f'Магазин "{self.name}": (Тревога, зафиксировано снижение цены!)\nГрандиозные скидки!!! Горячее '
+                      f'предложение!!!\n{item} по скидке в {discount}%!!!'
+                      f'\nТеперь всего-лишь за {price}!!!')
+            elif self.items[item] == price:
+                print(f'Магазин "{self.name}": цена на "{item}" осталась прежней - {self.items[item]}')
+            else: print(f'Магазин "{self.name}": новая цена на "{item}" - {price}')
+        else: print(f'Магазин "{self.name}": товар с наименованием "{item}" отсутствует в ассортименте.')
 
 
 
