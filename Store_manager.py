43c0b650ae388df105e82558b4ce0f3a87b3b9c9
Antonio <antonpogoryelov@gmail.com>
2024-06-27 14:49:08 +0300

    3 Shops
    3 objects class Store was created
    3 dicts with items: price

--- Store_manager.py
+++ Store_manager.py
@@ -16,11 +16,13 @@ class Store:
         if item in self.items:
             del self.items[item]
             print(f'Магазин "{self.name}": товар "{item}" удалён из ассортимента.')
-        else: print(f'Магазин "{self.name}": так товара с названием "{item}" и не было в наличии.')
+        else:
+            print(f'Магазин "{self.name}": так товара с названием "{item}" и не было в наличии.')
 
     def items_list(self):
         print(f'Магазин "{self.name}", вот что мы предлагаем нашим преданным клиентам:')
-        if self.items is None: print('Упс, пока что ничего:(')
+        if self.items is None:
+            print('Упс, пока что ничего:(')
         else:
             for item, price in self.items.items():
                 print(f'{item} - {price}')
@@ -42,11 +44,22 @@ class Store:
                       f'\nТеперь всего-лишь за {price}!!!')
             elif self.items[item] == price:
                 print(f'Магазин "{self.name}": цена на "{item}" осталась прежней - {self.items[item]}')
-            else: print(f'Магазин "{self.name}": новая цена на "{item}" - {price}')
-        else: print(f'Магазин "{self.name}": товар с наименованием "{item}" отсутствует в ассортименте.')
+            else:
+                print(f'Магазин "{self.name}": новая цена на "{item}" - {price}')
+        else:
+            print(f'Магазин "{self.name}": товар с наименованием "{item}" отсутствует в ассортименте.')
 
 
+test_list = {'Молоко': 60, 'Сыр': 300, 'Яблоки': 90, 'Бананы': 50, 'Шоколад': 100, 'Кофе': 400, 'Чай': 150,
+             'Мясо': 350, 'Рыба': 400}
+magic_list = {'Волшебная палочка': 1200, 'Кристалл предсказаний': 3000, 'Набор алхимика': 2500,
+              'Мантия невидимости': 4500, 'Зелье мудрости': 800}
+space_list = {'Гипердвигатель': 150000, 'Квантовый компьютер': 75000, 'Термальный щит': 50000,
+              'Модуль искусственной гравитации': 120000, 'Система жизнеобеспечения': 40000}
 
+local_store = Store(items=test_list)
+magic_store = Store('Чародейские Заморочки', 'Улица Волшебников, дом 17', magic_list)
+space_store = Store('Космо-Комплект', 'Улица Гагарина 42', space_list)
 
 
 
