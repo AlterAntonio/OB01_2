e09e133213ca18e2b50440f76ea7de60046be345
Antonio <antonpogoryelov@gmail.com>
2024-06-27 13:30:45 +0300

    add_item
    add_ was added

--- Store_manager.py
+++ Store_manager.py
@@ -3,4 +3,14 @@ class Store():
         self.name = name
         self.address = address
         self.items = items
-        
+
+
+    def add_item(self, item, price=0):
+        if item not in self.items.keys():
+            self.items[item] = price
+            print(f'Магазин "{self.name}: товар "{item}" добавлен в ассортимент по цене {price}')
+        else:
+            print(f'Магазин "{self.name}": товар с наименованием "{item}" уже есть в ассортименте по цене '
+                  f'{self.items[item]}')
+
+    
